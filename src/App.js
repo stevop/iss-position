d3efa09f28f73a700471a676c52bfdf5988059fa
StefanPacola <stevo.pacola@gmail.com>
2022-12-28 15:36:14 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+const App = () => {
+	const url = "http://api.open-notify.org/iss-now.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const [lat, setLat] = useState("");
+	const [lon, setLon] = useState("");
+	const [urlMap, setUrlMap] = useState("");
+
+	const getCoordinates = async () => {
+		const response = await fetch(url);
+		const data = await response.json();
+		setLat(data["iss_position"]["latitude"]);
+		setLon(data["iss_position"]["longitude"]);
+		setUrlMap(
+			`https://sk.mapy.cz/zakladni?x=${data["iss_position"]["latitude"]}&y=${data["iss_position"]["longitude"]}&z=5`
+		);
+	};
+	useEffect(() => {
+		getCoordinates();
+	}, []);
+
+	return (
+		<div>
+			<h1>API</h1>
+			<h2>Latitude</h2>
+			<p>{lat}</p>
+			<h2>Longitude</h2>
+			<p>{lon}</p>
+			<a
+				target="_blank"
+				href={urlMap}>
+				Odkaz na polohu ISS
+			</a>
+		</div>
+	);
+};
 
 export default App;
